693939fca969de40bc36f2042faaa1f8d9ae4839
Artur Saiakhov <pokermenu@gmail.com>
2022-05-11 11:31:50 +0300

    removed check

--- multiplyNumeric.js
+++ multiplyNumeric.js
@@ -1,19 +1,17 @@
-// Создайте функцию multiplyNumeric(obj), которая умножает все числовые свойства объекта obj на 2.
-// до вызова функции
-let menu = {
-width: 200,
-height: 300,
-title: "My menu"
-};
-
-multiplyNumeric(menu);
-function multiplyNumeric(object) {
-    for (const key in object) {
-        if (typeof object[key] === "number") {
-            object[key] *= 2;
-            console.log(object[key]);
-            
+    // Создайте функцию multiplyNumeric(obj), которая умножает все числовые свойства объекта obj на 2.
+    // до вызова функции
+    let menu = {
+        width: 200,
+        height: 300,
+        title: "My menu"
+        };
+    
+        multiplyNumeric(menu);
+        function multiplyNumeric(object) {
+            for (const key in object) {
+                if (typeof object[key] === "number") {
+                    object[key] *= 2;
+                }
+            }
         }
-    }
-}
-console.log(menu);
+        console.log(menu);
